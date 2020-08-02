11cd4879656146076bb2c7f16ae8dc2d78cbfee6
VioleSean <56226359+VioleSean@users.noreply.github.com>
2020-08-02 14:04:11 +0700

    Update server.js

--- commands/server.js
+++ commands/server.js
@@ -1,18 +1,9 @@
 module.exports = {
-	name: 'prune',
+	name: 'server',
   cooldown: 5,
-	description: 'prune.',
+	description: 'SERVER INFO',
 	execute(message, args) {
-    const amount = parseInt(args[0]);
-    if (isNaN(amount)) {
-		  return message.reply('that doesn\'t seem to be a valid number.');
-    }
-    else if (amount < 1 || amount > 100) {
-	    return message.reply('you need to input a number between 1 and 100.');
-    }
-    message.channel.bulkDelete(amount, true).catch(err => {
-	  console.error(err);
-	  message.channel.send('there was an error trying to prune messages in this channel!');
-    });
+		return message.channel.send(`>>> **SERVER INFO**\n**-----------------**\n**Name SERVER**\n\`${message.guild.name}\`\n**Total Member**\n         \`${message.guild.memberCount}\``);     
 	},
 };
+    
