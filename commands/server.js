c88cf38ba3256211e3366b4de56b467dbbe3d450
VioleSean <56226359+VioleSean@users.noreply.github.com>
2020-08-04 00:36:22 +0700

    Delete server.js

--- commands/server.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-	name: 'server',
-  cooldown: 5,
-	description: 'SERVER INFO',
-	execute(message, args) {
-		return message.channel.send(`>>> **SERVER INFO**\n**-----------------**\n**Name SERVER**\n\`${message.guild.name}\`\n**Total Member**\n         \`${message.guild.memberCount}\``);     
-	},
-};
-    
