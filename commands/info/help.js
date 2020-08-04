b0d40447483a1ece1f290aca02862efe1cf47f0f
VioleSean <56226359+VioleSean@users.noreply.github.com>
2020-08-04 09:30:17 +0700

    Update help.js

--- commands/info/help.js
+++ commands/info/help.js
@@ -29,7 +29,7 @@ module.exports = {
       const commands = await client.commands;
 
       let emx = new MessageEmbed()
-        .setDescription(`List all command`)
+        .setDescription(`\**List all command**\`)
         .setColor("GREEN")
         .setThumbnail(client.user.displayAvatarURL())
         .setTimestamp()
