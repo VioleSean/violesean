d410c832dea4849a28acbdbe1b458a1f32b2d5c8
VioleSean <56226359+VioleSean@users.noreply.github.com>
2020-08-10 07:50:23 +0700

    Add files via upload

---
+++ commands/info/ping.js
@@ -0,0 +1,17 @@
+const Discord = require("discord.js");
+module.exports = {
+  name: "ping",
+  category: "info",
+  description: "Returns latency and API ping",
+  run: async (client, message, args) => {
+    message.channel.send(`🏓 Pinging . . . .`).then((msg) => {
+      const _ = new Discord.MessageEmbed()
+        .setTitle("🏓Pong🏓")
+        .setDescription(`🏓🏓🏓\nLatency is \*\*${Math.floor(msg.createdTimestamp - message.createdTimestamp)}\*\* MS\nAPI Latency is \*\*${Math.round(bot.ws.ping)}\*\* MS\n🏓🏓🏓`)
+        .setColor("RANDOM");
+      msg.edit(_);
+      msg.edit("\u200B");
+    });
+  },
+};
+//Pong ping !!!
