f89b8809ac11385c3a9c3761d7a2c7b97c1cdcf3
VioleSean <56226359+VioleSean@users.noreply.github.com>
2020-08-10 09:11:34 +0700

    Update imdb.js

--- commands/info/imdb.js
+++ commands/info/imdb.js
@@ -24,7 +24,7 @@ name: "imdb",
     .setFooter(`⭐Ratings: ${movie.rating}`)
     .addField("Country", movie.country, true)
     .addField("Languages", movie.languages, true)
-    .addField("Episodes", movie.episode, true)
+    .addField("Release", movie.release, true)
     .addField("Type", movie.type, true);
     
     
